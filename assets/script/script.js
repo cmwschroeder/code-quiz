1695a743365c06c33c4b37c20f06dd88dc2724ec
cmwschroeder <cmwschroeder@gmail.com>
2022-03-31 23:09:50 -0700

    started an array of objects that will hold quiz questions, put first two questions in

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,6 +3,21 @@ var timerEl = document.getElementById("timer");
 var startEl = document.querySelector("#start");
 var introEl = document.querySelector("#intro");
 
+
+var quiz = [
+    {
+        question: "Which tag would you use to redirect the user to another page when clicked?",
+        correctAnswer: "a",
+        incorrectAnswer: ["p", "link", "ul"]
+    },
+    {
+        question: "Which attribute would you use to round the corners of an element?",
+        correctAnswer: "border-radius",
+        incorrectAnswer: ["transformation", "transition", "border"]
+    }
+];
+
+
 var timeLeft = 60;
 
 function countdown() {
